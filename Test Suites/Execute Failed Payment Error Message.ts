ffe7c4b4e1ebcdea5c0fa9af076979d450c9c64d
pdeguzman <pdeguzman@172.22.54.93>
2026-01-22 20:27:39 +0800

    Adding Test Suites

---
+++ Test Suites/Execute Failed Payment Error Message.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Execute Failed Payment Error Message</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7db76197-a31b-4677-a394-6026a566daf8</testSuiteGuid>
+   <testCaseLink>
+      <guid>ba5a4669-8c8b-4248-b155-33c2027e4ca3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Failed Payment Error Message</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
